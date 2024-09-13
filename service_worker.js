15add654b320a95d9ce0f51f178e0afde1d32e50
Steven Moses <soggsteven92@gmail.com>
2024-09-14 05:21:43 +0530

    working again

--- service_worker.js
+++ service_worker.js
@@ -2,7 +2,7 @@
 var screenshotImage = ""
 chrome.runtime.onMessage.addListener(async function (request, sender, sendResponse) {
     if (request.type == "screenshot") {
-        var url = chrome.runtime.getURL("./webpack/src/index.html");
+        var url = chrome.runtime.getURL("index.html");
         screenshotImage = await chrome.tabs.captureVisibleTab();
         var tab = chrome.tabs.create({ url: url });
         sendResponse({ success: true });
